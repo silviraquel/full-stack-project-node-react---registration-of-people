868e7f12af1c95bc8bc3bdd28ecd02a221146d83
Silviane Bezerra <silvianeraquel@gmail.com>
2022-06-20 11:28:51 -0300

    refatorando_routes_p_uma_linha_d_import

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,5 @@
 import express from 'express';
 import controllersperson from '../src/controllers/controllersperson.js';
-import findAll from '../src/controllers/controllersperson.js';
-import findPerson from '../src/controllers/controllersperson.js';
-import addPerson from '../src/controllers/controllersperson.js';
-import updateperson from '../src/controllers/controllersperson.js';
-import deleteperson from '../src/controllers/controllersperson.js';
-
 
 const routes = express.Router();
 
