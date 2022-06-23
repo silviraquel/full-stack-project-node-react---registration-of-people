535e2e90576a7bcc38d2b1b5fde396ad965fc2fe
Silviane Bezerra <silvianeraquel@gmail.com>
2022-06-23 09:43:01 -0300

    trazer_cpf_sem_mascara_pro_banco

--- src/controllers/controllersperson.js
+++ src/controllers/controllersperson.js
@@ -18,33 +18,36 @@ function findAll(req, res) {
 
   async function addPerson(req, res) {
 
+    
+    let cleanCpf = req.body.cpf.replace(/\D/g,'');
+    
     const validate = validator({ format: 'YYYY-MM-DD' })
     
     if(!validate(req.body.birthday)){
       return res.status(400).json({message: "birthday-format inválido"})
     }
-
+    
     const dataAtual= new Date().getTime()
     const data = new Date(req.body.birthday).getTime()
-      if (data > dataAtual) {
+    if (data > dataAtual) {
       return res.status(400).json({message: "Esta data não pode ser futura!"})
-      }
- 
-      if(!isCpf(req.body.cpf)){
-        return res.status(400).json({message:"CPF inválido"});
-      }
-
-      const cpfExists = await Modelperson.findOne({
-        where:({cpf:req.body.cpf})
-      });
-        if(cpfExists){
-         return res.status(400).json({message:"Esse CPF já foi cadastrado"})
-       }
-     
-      Modelperson.create({
+    }
+    
+    if(!isCpf(cleanCpf)){
+      return res.status(400).json({message:"CPF inválido"});
+    }
+    
+    const cpfExists = await Modelperson.findOne({
+      where:({cpf:cleanCpf})
+    });
+    if(cpfExists){
+      return res.json({message:"Esse CPF já foi cadastrado"})
+    }
+    
+    Modelperson.create({
       name: req.body.name,
       birthday: req.body.birthday,
-      cpf: req.body.cpf,
+      cpf: cleanCpf,
       createdAt: req.body.createdAt,
       updatedAt:req.body.updatedAt
     }).then((result) => res.json(result));
