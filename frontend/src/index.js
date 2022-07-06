04579288e3b1ee0116cd3500cc6b4906935b93b5
Silviane Bezerra <silvianeraquel@gmail.com>
2022-07-06 11:38:28 -0300

    instalacao_template_frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render } from "react-dom";
+import { 
+  BrowserRouter, 
+  Routes, 
+  Route 
+} from "react-router-dom";
+
+import './index.css';
+import App from './App';
+
+import Login from './pages/Login';
+import Author from './pages/Author';
+import Home from './pages/Home';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+
+const rootElement = document.getElementById("root");
+
+render(
+  <BrowserRouter>
+    <Routes>
+
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="authors" element={<Author />} />
+        <Route path="*" element={
+          <h4>Página não encontrada!</h4>
+        } />
+      </Route>
+      <Route path="/login" element={<Login />} />
+
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
+
---
+++ frontend/src/pages/Author/index.js
@@ -0,0 +1,3 @@
+export default function Author(){
+  return <>página do author</>;
+}
