d6b2f4d63ea1f533a8508a10a1511b9a54a04336
Silviane Bezerra <silvianeraquel@gmail.com>
2022-07-06 12:42:38 -0300

    axios.get_react_17

--- frontend/src/Layout/Header/index.js
+++ frontend/src/Layout/Header/index.js
@@ -4,19 +4,15 @@ export default function Header(){
   return (
     <>
       <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <Link className="navbar-brand col-md-3 col-lg-2 me-0 px-3" to="/">Company name</Link>
+        <Link className="navbar-brand col-md-3 col-lg-2 me-0 px-3" to="/">Silvy's</Link>
         <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
-
-        <div className="navbar-nav">
-          <div className="nav-item text-nowrap">
-            <Link className="nav-link px-3" to="/login">Sign out</Link>
-          </div>
-        </div>
-      </header>
+        <div className="mx-auto order-0">
+        <a className="navbar-brand mx-auto">Registration of People</a>
+    </div>      
+    </header>
     </>
   );
 }
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,5 +1,65 @@
-export default function Index() {
-  return (
-    <>página inicial</>
-  );
-}
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+
+
+const Index = () => {
+
+  const [person, setPerson] = useState([]);
+
+      useEffect(() => {
+        
+        axios.get('/person').then(response => {
+          setPerson(response.data);
+        });
+
+     }, [])
+
+    return (
+      <>
+        <Container className="topo" class='d-flex align-items-center  flex-direction: column align-items-stretch padding-top'>
+          <Table>
+            <thead>
+
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Birthday</th>
+                <th>CPF</th>
+                <th>Opções</th>
+              </tr>
+
+            </thead>
+            <tbody>
+
+               {person.map((plp) => (
+                  <tr key ={plp.id}>
+                  <td>{plp.id}</td>
+                  <td>{plp.name}</td>
+                  <td>{plp.birthday}</td>
+                  <td>{plp.cpf}</td>
+                  <td>
+                    <Button size="md" variant="primary">EDITAR</Button>
+                    <Button size="md" variant="danger">EXCLUIR</Button>
+                  </td>
+                  </tr>
+                 ))}    
+            </tbody>
+          </Table>
+        </Container>
+      </>
+    )
+  }
+
+export default Index;
+
+
+
+// export default function Index() {
+//   return (
+//     <>página inicial</>
+//   );
+// }
